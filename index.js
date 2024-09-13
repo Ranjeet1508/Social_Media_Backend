20ddc9768eb10b688beedbf50abbb3b85fb7f8dc
Ranjeet1508 <danavrj1998@gmail.com>
2024-09-13 11:24:23 +0530

    revived backend

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ app.use(cors({
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({limit: '5mb', extended: true}));
 
-
+// Root route handler
+app.get('/', (req, res) => {
+    res.send('Welcome to the API Home Route!');
+});
 
 app.use('/user', userRouter)
 app.use('/post',postRouter);
